ab8f3ccc1e198c01dca5c151d83d8b3fd0431271
wenhaowu <wenhao293h@gmail.com>
2015-12-30 16:15:52 +0200

    Demo pictures

--- app/src/main/java/wenhao/practice/bingwallpaper/View_MainActivity.java
+++ app/src/main/java/wenhao/practice/bingwallpaper/View_MainActivity.java
@@ -37,6 +37,7 @@ public class View_MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle("Gallery");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
